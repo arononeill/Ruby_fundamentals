bb49148b69a6f9082b11411e737c55c87448358a
Aron O'Neill <36157615+arononeill@users.noreply.github.com>
2019-07-04 11:31:46 +0100

    method declaration examples

--- methods/keyword_arguments.rb
+++ methods/keyword_arguments.rb
@@ -1,6 +1,8 @@
 # A program to highlight how to use keyword arguments in a function
 
 
+# Here, the elements of custom_components are given default values and whatever needs to be given a different
+# value, can be done when the function is called
 def produce_spaceship(type = :firefighter, size: :m, **custom_components)
   components = {engine: :standard,
                 seats: :standard,
@@ -9,5 +11,6 @@ def produce_spaceship(type = :firefighter, size: :m, **custom_components)
   components.merge!(custom_components)
 end
 
+# Whatever custom component parts differ to the default values, are defined when the function is called
 produce_spaceship(:yacht, size: :s, engine: :rolls_royce, seats: :leather)
 
