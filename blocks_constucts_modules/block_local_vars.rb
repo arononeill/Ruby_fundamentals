d8640de1ee5891114ee6b53e17cfc8e917beeb60
Aron O'Neill <36157615+arononeill@users.noreply.github.com>
2019-07-04 15:38:45 +0100

    exploring uses of blocks

---
+++ blocks_constucts_modules/block_local_vars.rb
@@ -0,0 +1,27 @@
+class Spaceship
+  def initialise
+    @debug = true
+    @debug_attrs = {containment_status: :ok, core_temp: 350}
+  end
+
+  def debug_only
+    return nil unless @debug
+    return nil unless block_given?
+    puts "Running debug code"
+    yield @debug_attrs
+  end
+
+  def locate_cargo
+    attrs = {weight: 10, destination: "Earth"}
+    debug_only do |d_attrs; attrs| # block local variable
+      attrs = {}
+      attrs[:cargo_accessed] = true
+    end
+
+    p attrs
+    p @debug_attrs
+  end
+end
+
+ship = Spaceship.new
+ship.locate_cargo
